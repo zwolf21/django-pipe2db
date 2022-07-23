e8928610d64f494c818e8172e0c0cb75a762fd12
zwolf21 <pbr112@naver.com>
2022-07-23 19:23:27 +0900

    test updated

--- test/bookstore/insert.py
+++ test/bookstore/insert.py
@@ -1,15 +1,57 @@
 from pipe2db import pipe, setupdb
-from samples import *
+
 
 setupdb()
 
+
+author1 = {
+    'email': 'xman1@google.com',
+    'first_name': 'charse',
+    'last_name': 'javie',
+    'date_of_birth': '1975-07-25',
+    'date_of_death': '1995-07-11'
+}
+author2 = {
+    'email': 'yman1@google.com',
+    'first_name': 'jin',
+    'last_name': 'gray',
+    'date_of_birth': '1925-07-25',
+    'date_of_death': '1999-01-21'
+}
+author3 = {
+    'email': 'batman1@google.com',
+    'first_name': 'wolverin',
+    'last_name': 'jack',
+    'date_of_birth': '1988-07-25',
+    'date_of_death': None
+}
+
+
+
+# create
+@pipe({
+    'model': 'db.Author',
+    'unique_key': 'email',
+})
+def insert():
+    yield author1
+    yield author2
+    yield author3
+
+
+
+# create or update
 @pipe({
     'model': 'db.Author',
     'unique_key': 'email',
     'method': 'update'
 })
 def insert():
-    yield author1
+    author1['first_name'] = 'updated'
     yield author2
     yield author3
+    yield author1
+
+
+
 
